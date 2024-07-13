ae8070628eca92cee3320d46c41227e46743f74a
dimi4 <31467280+dimi4ik@users.noreply.github.com>
2024-07-13 12:20:08 +0200

    fead: fix readme.md

--- main.tf
+++ main.tf
@@ -11,9 +11,5 @@ resource "azurerm_resource_group" "main_rg" {
   location = var.location
   tags     = local.merged_tags
 }
-
-resource "azurerm_resource_group" "main_rg2" {
-  name     = local.resource_group_name
-  location = var.location
-  tags     = local.merged_tags
-}
+ 
+ 
