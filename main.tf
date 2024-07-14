329bd418e84ec99d1213e161a435c44a9eee491b
Dima Lejkin <dmitrij.lejkin@abraxas.ch>
2024-07-14 19:45:57 +0200

    add_new_files

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 ################################################################
 # Resource Group                                               #
-# 							                                               #
+# 							       #
 # This resource block defines an Azure Resource Group.         #
 # It creates a resource group with the specified name,location #
-# and tags.  						                                       #
+# and tags.  						       #
 #################################################################
 
 resource "azurerm_resource_group" "main_rg" {
@@ -11,5 +11,9 @@ resource "azurerm_resource_group" "main_rg" {
   location = var.location
   tags     = local.merged_tags
 }
- 
- 
+
+resource "azurerm_resource_group" "main_rg2" {
+  name     = local.resource_group_name
+  location = var.location
+  tags     = local.merged_tags
+}
