43e391e1a3e03e7655c9974c86e633df0ffa3eb7
dimi4 <31467280+dimi4ik@users.noreply.github.com>
2025-05-08 04:44:07 +0000

    Migrate Terraform state backend from Azure Storage to GitLab HTTP

--- main.tf
+++ main.tf
@@ -6,6 +6,20 @@
 # and tags.  						       #
 #################################################################
 
+terraform {
+  backend "http" {
+    address        = "https://gitlab.com/api/v4/projects/<PROJECT_ID>/terraform/state/<STATE_NAME>"
+    lock_address   = "https://gitlab.com/api/v4/projects/<PROJECT_ID>/terraform/state/<STATE_NAME>/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/<PROJECT_ID>/terraform/state/<STATE_NAME>/lock"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    username       = "gitlab-user"
+    password       = "gitlab-token"
+  }
+}
+
+# Hinweis: Ersetzen Sie <PROJECT_ID> und <STATE_NAME> durch die entsprechenden Werte.
+
 resource "azurerm_resource_group" "main_rg" {
   name     = local.resource_group_name
   location = var.location
